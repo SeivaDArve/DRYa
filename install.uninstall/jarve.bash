0653b5cfed617bb2f7e15648f8a96fc0edbd00be
Seiva D'Arve <flowreshe.seiva.d.arve@gmail.com>
2022-02-07 16:30:48 +0000

    created the first good menu for DRYa

--- install.uninstall/jarve.bash
+++ install.uninstall/jarve.bash
@@ -2,24 +2,33 @@
 # Title: install jarve at your home folder (~/.jarve)
 # Description: Installing means copy files to ~/.bashrc. In the future you can run it on windows, android and ios
 # Mothod: Prompting a menu to choose
-# Displaying ASCII logo for jarve. running jarve.
-cat ../all/.jarve/all/img/CLI/jarve-icon.txt
 # Also in future: display whoami, and give a special ID for the MACHINE, so that it can be listed on jarve-subPocket. the machine, username and id numer, as well as jarve-icon.txt should run at the level of .bashrc"
 
 # Menu
-function Menu {
+function menu {
 clear
-cat << heredoc-menu
+echo " 
  _____   _____  __     __       
 |  __ \ |  __ \ \ \   / /       
 | |  | || |__) | \ \_/ /   __ _ 
-| |  | ||  _  /   \   /   / _` |
+| |  | ||  _  /   \   /   / _\` |
 | |__| || | \ \    | |   | (_| |
 |_____/ |_|  \_\   |_|    \__,_|
                                 
-Hi, This is the menu for DRYa (Don't Repeat Yourself app)
-heredoc-menu
+(Don't Repeat Yourself app) - Menu
 
+Asking user: Do you want to:
+1) Install	|
+2) Re-install	| Software
+3) Remove	|
+4) Track	|
 
-# Asking user: Do you want to Intall, Re-install or Remove?
+5) Instructions
+6) Exit
+"
+read answer
 }
+
+
+
+menu
