dadc79569edb81907435c533fc16300bc7979534
seivadarve <seiva.d.arve@gmail.com>
2022-03-31 12:37:21 +0100

    update drya.sh

--- drya.sh
+++ drya.sh
@@ -2,35 +2,55 @@
 
 # Max Cols: 56; Max lines: 28 (to aprox match smartphone screen)
 
-function horizline {
+_slide=0
+	
+function f_slide {
+	((_slide=_slide+1))
+	echo $_slide
+}
+
+function f_horizline {
 _count=$(tput cols)
 for i in $(seq $_count); do
    echo -ne "-" 
 done
 }
 
-function replaceRead {
+function f_replaceRead {
 echo "First line..."
 tput sc
 read -p "Press any key to overwrite this line... " -n1 -s
 tput rc 1; tput el
 echo "Second line. read replaced."
 }
-function mainmenu {
+
+function f_wiki {
+cat << heredoc
+To undo "tput rev" use "tput sgr0"
+heredoc
+}
+
+function f_mainmenu {
 tput clear
-tput rev # To undo "tput sgr0"
 tput home; horizline; 
 
 tput cup 0 2
 echo --- Menu DRYa ---
 
+#f_entry1
+#f_entry2
+#f_entry3
+#f_entryStop
+
 tput cup 28 0; horizline
 }
 
 
-function exec {
-mainmenu
-read
-replaceRead
+function f_exec {
+#f_mainmenu
+echo $_slide
+f_slide
+echo $_slide
+f_slide
 }
-exec
+f_exec
