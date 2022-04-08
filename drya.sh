b16c4a8aaba41c560c295aca29fcccc50ad9afd9
seivadarve <seiva.d.arve@gmail.com>
2022-04-08 13:49:55 +0100

    update drya.sh

--- drya.sh
+++ drya.sh
@@ -2,25 +2,76 @@
 
 clear
 
-# Max Cols: 56; Max lines: 28 (to aprox match smartphone screen)
-
-# Decide 2 sliders, one Horizontal and one Vertical
-_V=0
-_H=0
+function f_default_vars {
+	# Max Cols: 56; Max lines: 28 (to aprox match smartphone screen)
 	
+	# Decide 2 sliders, one Horizontal and one Vertical
+	_V=0; _H=0
+		
+	# Decide a variable to control the page number of current menu being displayed
+	_m=0
+}
+
+function f_cursorON {
+	# Show cursor normally. Use "tput civis" to hide
+	tput cnorm
+}
+
+function f_cursorOFF {
+	# Hide cursor to prevent flickering of the screen. Use "tput cnorm" to show again
+	tput civis
+}
+
 function f_ascii_icon {
 
-	function f_center_to_screen {
-		tput cols
-		tput lines
+	function f_center_to_screen_verbose {
+		# To messure the screen width:
+		_cols=$(tput cols)
+		echo Total cols: $_cols
+
+		# To messure hslf the width of the screen:
+		_cols=$((_cols/2))
+		echo Half the sreen is: $_cols
+
+		# The ascii logo is 32 characters long (half is 16)
+		echo "The ascii logo is 32 characters long (half is 16)"
+
+		# Center to screen
+		echo To center the logo horizontally to the screen we subtract half of the logo cols to half of the screen widht. And that is where we center our cursor
+
+
+		_cols=$((_cols-16))
+		echo -e "\nRemaining cols: $_cols"
+		echo $_cols Is where we put our cursor
+
+
+		echo Example of logo widht:
+		echo "................................"
+
+		echo -e "\nExample of logo centered:"
+		tput cup 13 $_cols
+		echo "................................"
+
+
+		# Now the same for lines:
+		#tput lines
+		
 	}
-	echo -e "     ||\`				"
-	echo "     ||				"
-	echo -e " .|''||  '||''| '||  ||\`  '''|.	"
-	echo -e " ||  ||   ||     \`|..||  .|''||	"
-	echo -e " \`|..||. .||.        ||  \`|..||.	"
-	echo "                  ,  |'		"
-	echo "                    ''		"
+	#f_center_to_screen_verbose
+
+	function f_spaces {
+		# Manually writting 12 spaces before the ascii logo like defined at f_center_of_screen_verbose
+		# Manually means: script not complete yet, it should detect the screen and act automated
+		echo -n "          "
+	}
+
+	f_spaces; echo -e "     ||\`				"
+	f_spaces; echo "     ||				"
+	f_spaces; echo -e " .|''||  '||''| '||  ||\`  '''|.	"
+	f_spaces; echo -e " ||  ||   ||     \`|..||  .|''||	"
+	f_spaces; echo -e " \`|..||. .||.        ||  \`|..||.	"
+	f_spaces; echo "                  ,  |'		"
+	f_spaces; echo "                    ''		"
 
 	sleep 1
 	tput sc 
@@ -55,6 +106,7 @@ function f_slideHdw {
 	#echo $_H
 }
 
+
 function f_setafA {
 	# This function is to be used when something is ASKED
 	tput setaf 4
@@ -69,10 +121,28 @@ function f_setafC {
 	# This function is to be used when styles are to be CLEARED
 	tput sgr0
 }
+
+function f_res_cursor {
+	tput cup 25 4
+}
+
+function f_clear_space {
+	# Repeat 24x
+	for i in {1..2}; do echo "hi"; done
+}
+
 function f_horizline {
 	_count=$(tput cols)
 	for i in $(seq $_count); do
-   	echo -ne "-" 
+	   	echo -ne "-" 
+	done
+}
+
+function f_verticline {
+	_count=$(tput lines)
+	for i in $(seq $_count); do
+   	echo -ne "    |\n" 
+	#sleep 0.5
 	done
 }
 
@@ -95,10 +165,16 @@ heredoc
 }
 
 function f_detectOS {
+	clear
+	f_setafD; echo Detect OS
+	f_setafC
 	echo "whoami: 	$(whoami)"
 	echo "OS type: 	${OSTYPE}"
 	echo "uname:		$(uname)"
 	echo "uname -a: 	$(uname -a)"
+	read
+
+	f_menu1
 }
 
 function f_readKeystroke {
@@ -108,45 +184,43 @@ function f_readKeystroke {
 		# If you run f_exec for the first time, this if statment will break the while loop allowing f_mainmenu to run for the first time without asking for any keystroke or changes
 		if [ ${_H} = 0 ] && [ ${_V} = 0 ]; then
 			#echo "no keystroke is asked"
-			_H=1
-			_V=1
-			break
+			_H=1;_V=1
+			f_menu1
 		fi
 			
 		read -rsn1 input
 		if [ "$input" = "s" ]; then
-    			#echo "key pressed: s"
+    			echo "key pressed: s"
+			sleep 1
 			#echo $_V
 			f_slideVdw
 			
 		fi
 
-		if [ "$input" = "b" ]; then
-    			echo "key pressed: b"
-		fi
-
-		if [ "$input" = "S" ]; then
-			# Exit the while loop
-			break
-		fi
+		if [ "$input" = "b" ]; then echo "key pressed: b"; fi
+		if [ "$input" = "S" ]; then echo ""; tput cnorm; exit; fi
+		if [ "$input" = "D" ]; then f_detectOS; fi
 
 		# Next it needs to recognize:
 		# -Arrow Keys
 		# -Enter
-
-		f_menu1
+		
 	done
+		f_menu1
 }
 
 function f_entryA1 {
 	# Reverse colors for this entry if it matches cursor
 	if [ ${_V} = 1 ]; then tput rev; fi
 
-	tput cup 2 4
+	tput cup 2 7
 	echo "Entry 1"
 
 	# Reset text format (if any)
 	tput sgr0
+
+	# Reset Cursor position
+	f_res_cursor
 }
 
 function f_entryA2 {
@@ -158,6 +232,9 @@ function f_entryA2 {
 
 	# Reset text format (if any)
 	tput sgr0
+
+	# Reset Cursor position
+	f_res_cursor
 }
 
 function f_reset_PS1 {
@@ -171,23 +248,41 @@ function f_footer {
 
 	tput rev
 
-	tput cup 23 11
-	echo Stop: S
+	# Arrows
+	tput cup 23 6
+	echo " <   ^   v   > "
+	tput cup 24 6
+	echo "(a) (w) (s) (d)"
 
-	tput cup 23 25
-	echo Detect OS: D
+	# Stop
+	tput cup 24 22
+	echo "(S)top"
 
-	tput cup 23 35
-	echo Detect OS: D
+	# Decect Device
+	tput cup 24 29
+	echo "(D)etect Device"
 
-	f_arrows
+	# Reset cursor to bottom
+	tput cup 25 4
 
 	tput sgr0
 }
 
 function f_menu1 {
+
+	# Change the menu variable for internal comunication
+	_m=1
+
+	tput cup 2 1
+	echo "M1"
+	echo " --"
+	echo " --"
+	echo " --"
+	echo " --"
+	echo " --"
+	f_res_cursor
 	f_entryA1
-	f_entryA2
+	#f_entryA2
 	#f_entryA3
 }
 
@@ -198,27 +293,30 @@ function f_menu1 {
 #}
 
 function f_mainmenu {
+	# Create a style for the menu:
 	tput clear
+	f_verticline
 	tput home; f_horizline; 
 	tput cup 25 0; f_horizline
-	
 	tput cup 0 3
 	echo " Menu DRYa "
 
 	f_footer
-	#f_menu1
-	#f_menu2
+	if [ $_m == 1 ]; then f_menu1; fi
+	#if [ $_m = 2 ]; then f_menu2; fi
+	#if [ $_m = 3 ]; then f_menu3; fi
+	#if [ $_m = 4 ]; then f_menu4; fi
+	#if [ $_m = 5 ]; then f_menu5; fi
+	#if [ $_m = 6 ]; then f_menu6; fi
+	#if [ $_m = 7 ]; then f_menu7; fi
+	#if [ $_m = 8 ]; then f_menu8; fi
+	#if [ $_m = 9 ]; then f_menu9; fi
+	#if [ $_m = 0 ]; then f_menu0; fi
 
-	#f_readKeystroke
+	f_readKeystroke
 	f_reset_PS1
 }
 
-function f_arrows {
-	tput cup 22 2
-	echo "< ^ v >"
-	tput cup 23 2
-	echo "a w s d"
-}
 
 function f_detect_dir_or_file {
 	# To check if a directory exists side by side with DRYa repo:
@@ -234,7 +332,9 @@ function f_detect_dir_or_file {
 	f_setafC
 		  echo "(1) jarve repo?"
 		  echo "(2) upK repo?"
-		  echo -e "(3) .vimrc file?\n"
+		  echo "(3) .vimrc file?"
+		  echo "(4) gitMenu"
+		  echo ""
 
 	read _ans
 
@@ -307,14 +407,19 @@ function f_get_script_current_abs_path {
 
 
 function f_exec {
-	f_ascii_icon
-	#f_detectOS
+	f_default_vars
+
+	f_cursorON
+	#f_cursorOFF
+
+	#f_ascii_icon
 	#f_mainmenu
 	#f_readKeystroke
 	#f_wiki
 	#f_menu1
-		#f_install_vimrc
+	#f_install_vimrc
 	#f_get_script_current_abs_path
-	f_detect_dir_or_file
+	source ../jarve/jrv/etc/usr-etc/termux-Dv/.jrvrc
+	#f_detect_dir_or_file
 }
 f_exec
