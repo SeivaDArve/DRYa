a8f49be41238330fae2764f7324401104d6e4076
seivadarve <seiva.d.arve@gmail.com>
2022-09-10 23:21:38 +0100

    drya will be able to install apps by layers: Vanilla first and then plug-ins. Due the fact drya can run only in terminal, icons were moved to directory "GUI" && Update: man-page

---
+++ install.uninstall/linux/drya-create-dryarc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Title: Update Me Locally
+# Description: executable that creates a file at ~/.config/h.h/drya/ called .dryarc which ia meant to store temporary configs for DRYa.
+# Use: drya-create-dryarc to create new .dryarc file ##Function is stored under .../DRYa/all/config-bash-alias
+# Use: drya-edit-tmp-config to add text to ~/.config/h.h/drya/.dryarc ## uDev: this function does not exiat yet
+# Use: drya-updateMe-Locally to append ~/.config/h.h/drya/.dryarc into ~/DRYa/all/config-bash-alias and create new empty ~/.config/h.h/drya/.dryarc BY CALLING THIS SCRIPT
+#cat upk.sh then search only for a function of greetings, then echo that out
+
+# Create new .dryarc
+# rm      ## uDev: this function already exist at drya.sh (you should copy it here. delete there. And there, call this function )
+
