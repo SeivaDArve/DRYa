94953ded5fe2a52ec1de7543242795d9df553f74
seivadarve <flowreshe.seiva.d.arve@gmail.com>
2022-09-24 04:56:54 +0100

    Update: installer.sh: now it uses the /bin script to insirt drya into ~/.bashrc

--- install.uninstall/linux/installer.sh
+++ install.uninstall/linux/installer.sh
@@ -15,32 +15,9 @@ function f_export_updated_dryarc {
 	cp ./future-dryarc ~/.config/h.h/.dryarcdrya
 }
 
-# Add .dryarc to .bashrc
 function f_add_dryarc_to_bashrc {
-	touch ~/.bashrc  # It updated the file. If it does not exist then it created it
-
-	# Test the file. If grep finds a pattern then "$? = 0". Otherwise if grep does not find a pattern then "$? = 1"
-	# This test just needs info at the variable $? and does not need the ouptup of a file. That is why we clear the output by redirecting "1>" to other place hidden "/dev/null"
-	grep "#!/bin/bash" ~/.bashrc 1>/dev/null
-	echo $? 1>/dev/null
-	echo $?
-
-		if [ $? == 0 ]; then echo "a shebang #!/bin/bash is visible inside ~/.bashrc"; fi
-		if [ $? == 1 ]; then echo '#!/bin/bash' >> ~/.bashrc; fi
-
-	# Bug: grep is not returning a correct value to $? and this way half of the installation is not possible.
-	# Fix: We need to know either if a line of code "source ~/.config/h.h/.dryarc" is written inside the file ~/.bashrc
-	# Hint: The $? variable is managed by the terminal itself and is used to tell the user either if the previous command was sucessfull or not
-	grep 'source ~/.config/h.h/.dryarc' ~/.bashrc 1>/dev/null
-	echo $? 1>/dev/null
-	echo $?
-		if [ $? == 0 ]; then echo ".dryarc is visible inside .bashrc"; fi
-		if [ $? == 1 ]; then echo "adding lines to ~/.bashrc to source all variables from .dryarc"
-				#echo "" >> ~/.bashrc
-				#echo '# Add visibility for .dryarc in this file' >> ~/.bashrc
-				#echo 'source ~/.config/h.h/.dryarc' >> ~/.bashrc
-				#echo "added source ~/.config/h.h/.dryarc to ~/.bashrc file"
-		fi
+   # Add .dryarc to .bashrc
+   bash ~/Repositories/DRYa/all/bin/re-direct-drya-to-bashrc
 }
 
 # Copy drya.desktop to...
@@ -55,6 +32,7 @@ function f_exec {
 	f_greet
 	f_export_updated_dryarc
 	f_add_dryarc_to_bashrc
+   #f_drya-to-desktop ##uDev: must be created
 	source ~/.bashrc && echo "sourced"
 }
 
