44e40beca1779d5db1d6b65e23b7641d25ae7af1
seivaDArve <flowreshe.seiva.d.arve@gmail.com>
2023-06-26 17:28:24 +0100

    update of a module for init.el: upk.el

--- all/dot-files/emacs/upk.el
+++ all/dot-files/emacs/upk.el
@@ -1,5 +1,7 @@
 ;; Title: File to store elisp functions for the org-mode files from upk users
 
-(defun upk-init ()
+(defun upk-init-test ()
    (interactive)
    (message "file found"))
+
+;; uDev: Maybe this link would help to setup this module: https://emacs.stackexchange.com/questions/47771/should-i-use-require-or-load-when-writing-my-own-configuration
