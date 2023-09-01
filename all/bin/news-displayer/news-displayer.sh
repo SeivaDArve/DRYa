c783027c42692bfefb2ff6d1b3066db4e4cea0a2
seivaDArve <flowreshe.seiva.d.arve@gmail.com>
2023-09-01 21:30:05 +0100

    Added function: news-displayer

---
+++ all/bin/news-displayer/news-displayer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Title: News Displayer
+# Description: Do you have an extra screen? (one tablet, TV with raspberry pi, Laptop etc..). If you have, then this script goes on using such screen to present to you the News that exist Around DRYa like: Moedaz tasks; TODO lists; Results of Web scrapping apps; Domotics status etc...
+# Use: Run this script and it will clear your screen and goes on presenting from 10s to 10s new data around DRYa activities
+
+source ${v_REPOS_CENTER}/moedaz/all/source-all-moedaz-files
