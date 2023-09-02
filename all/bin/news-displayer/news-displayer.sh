5e5812034001a9a33b179e7111bdc71986f124f7
seivaDArve <flowreshe.seiva.d.arve@gmail.com>
2023-09-02 19:10:57 +0100

    Upgrade: news-displayer

--- all/bin/news-displayer/news-displayer.sh
+++ all/bin/news-displayer/news-displayer.sh
@@ -4,4 +4,55 @@
 # Description: Do you have an extra screen? (one tablet, TV with raspberry pi, Laptop etc..). If you have, then this script goes on using such screen to present to you the News that exist Around DRYa like: Moedaz tasks; TODO lists; Results of Web scrapping apps; Domotics status etc...
 # Use: Run this script and it will clear your screen and goes on presenting from 10s to 10s new data around DRYa activities
 
-source ${v_REPOS_CENTER}/moedaz/all/source-all-moedaz-files
+
+function f_horizontal_line {
+   # This function calculates the amount of line present in the terminal window for the current zoom and creates an horizontal line across the screen
+
+         v_count="$COLUMNS"
+            #echo -e "There are currently $v_cols columns in the screen \n and from that number, $v_count is the\n number of dashes '-' that the menu will have "
+            #read
+
+         # You may choose the apropriate symbol here
+            v_underscore="-"
+
+         # Store in a var, how many dashes can be replaced by empty spaces (according to the specific amount of available columns)
+            v_underscoreCount=""
+
+            for e in $(seq $v_count); do 
+               v_underscoreCount="$v_underscoreCount$v_underscore"
+            done
+
+         # The result is an horizontal line
+            #echo "var is $v_underscoreCount"
+            #read
+            v_line=$v_underscoreCount
+
+            # removing last 4 characters from v_line. This way it can be used in SELECT menus
+               v_line2=${v_line::-4}
+
+            echo $v_underscoreCount
+}
+
+
+
+while true; do
+# source ${v_REPOS_CENTER}/moedaz/all/source-all-moedaz-files
+
+clear
+figlet News
+echo "drya: currently displaying NEWS"
+echo " > [ Any key ] to Pause/Play"
+echo " > [ Ctrl-C  ] to Stop"
+f_horizontal_line
+echo
+read -s -n 1 -t 5 v_play_pause
+
+if [ -z $v_play_pause ]; then 
+   echo "var empty, proceed"; sleep 0.5;
+
+else 
+   echo "var not empty, pause"; sleep 0.5; 
+   read -s -n 1
+fi
+
+done
