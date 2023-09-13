784e6d7e54a3e045d33d5862b873dd8dcb436957
seivaDArve <flowreshe.seiva.d.arve@gmail.com>
2023-09-13 12:02:06 +0100

    Added files fur further development

---
+++ all/bin/init-bin/nan-D/nanD-search-and-apply.sh
@@ -0,0 +1,4 @@
+This script should search the entire file system and locate files that contain instructions of changes to be applied
+
+example: 
+   write "cp ./ $my_backups" on a file called .D in at some directory on your file system. And when nan-D locates it, applies what is written in it
