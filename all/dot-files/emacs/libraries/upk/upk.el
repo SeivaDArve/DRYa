1f3e42a0b693ba156f3c5e3fcf6a919bb357a66b
seivaDArve <flowreshe.seiva.d.arve@gmail.com>
2023-09-23 13:31:07 +0100

    changed text only

--- all/dot-files/emacs/libraries/upk/upk.el
+++ all/dot-files/emacs/libraries/upk/upk.el
@@ -1,7 +1,9 @@
 ;; Title: File to store elisp functions for the org-mode files from upk users
 
-(defun test28 ()
-   (interactive)
-   (message "file found"))
+;;(defun test28 ()
+;;   (interactive)
+;;   (message "file found"))
+
+(message "file upk.el found")
 
 ;; uDev: Maybe this link would help to setup this module: https://emacs.stackexchange.com/questions/47771/should-i-use-require-or-load-when-writing-my-own-configuration
