d4f9df43e9391ac13b2863a1e0fde07ecbe622ef
seivaDArve <flowreshe.seiva.d.arve@gmail.com>
2023-10-05 17:55:08 +0100

    Changed text only

--- all/dot-files/emacs/libraries/upk/upk.el
+++ all/dot-files/emacs/libraries/upk/upk.el
@@ -2,8 +2,8 @@
 
 (defun dv-test ()
    (interactive)
-   (message "file found upk.el"))
+   (message "Dv: file found upk.el"))
 
-(message "dv: file upk.el found")
+(message "Dv: file upk.el found")
 
 ;; uDev: Maybe this link would help to setup this module: https://emacs.stackexchange.com/questions/47771/should-i-use-require-or-load-when-writing-my-own-configuration
