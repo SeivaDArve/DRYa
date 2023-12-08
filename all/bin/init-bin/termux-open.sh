24473c255a902909f768c6422f84eca8ceb5d227
seivaDArve <flowreshe.seiva.d.arve@gmail.com>
2023-12-08 07:17:32 +0000

    Tentative de usar a FX termux-open de forma cross-platform

---
+++ all/bin/init-bin/termux-open.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Title: termux-open
+# Description: grupo de funcoes facilitadoras ao termux-open
+# Use: Para usar este script: faz source para o $ENV e no emacs (ou outros) podes terntar abrir qualquer imagem apartir do termux. Este script é necessario quando é preciso descobrir quel é o OS em vigor (WSL, Linux, Android) e apartir desse OS abrir a nossa imagem
+
+   function f_attempt_WSL2 {
+     wslview ${v_REPOS_CENTER}/moedaz/all/real-documents/cartoes-supermercado/$v_name 2>/dev/null
+   }
+   
+   function f_attermpt_termux {
+     termux-open ${v_REPOS_CENTER}/moedaz/all/real-documents/cartoes-supermercado/$v_name 2>/dev/null
+   }
+   
+   function f_attempt_linux {
+     xdg-open ${v_REPOS_CENTER}/moedaz/all/real-documents/cartoes-supermercado/$v_name 2>/dev/null
+   }
+   
+   function f_op {
+     # Find wich OS is running and run just that
+     f_attempt_WSL2 || f_attempt_termux || f_attempt_linux
+   }
+   
+   function f_sure_open {
+     v_name_example="cartao.jpg"
+     v_name_example="/tmp/"
+     #v_name="<define-your-img-name-externally-first,it-will-replace-text-here>"
+     #v_name_example="/tmp/"
+     f_op
+   }
