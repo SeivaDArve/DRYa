a2ee8a96258f17efdfa61faca65b4f966f67d0f8
seivaDArve <flowreshe.seiva.d.arve@gmail.com>
2024-05-02 13:23:24 +0100

    Pushed to github.com automatically by ezGIT app

--- all/bin/jarve-sentinel.sh
+++ all/bin/jarve-sentinel.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 echo "Jarve running (uDev)"
+echo " > type 'D clone try jarve' to download my repo"
+echo " >> Use DRYa for shortcuts and commands"
+echo " >> Use Jarve to decide what commands to run next"
 
