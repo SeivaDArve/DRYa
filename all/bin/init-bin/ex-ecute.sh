42d841cd2c76214c3fa57ad9f04aec14e33062f9
seivaDArve <flowreshe.seiva.d.arve@gmail.com>
2024-05-13 01:22:54 +0100

    Pushed to github.com automatically by ezGIT app

--- all/bin/init-bin/ex-ecute.sh
+++ all/bin/init-bin/ex-ecute.sh
@@ -14,7 +14,23 @@ function ex {
       # uDev: Se houver mais do que um ficheiro: questionar qual o utilizador quer
       # uDev: atender à extensao do ficheiro, se for .py entao abre com python
       # uDev: vao existir numeros 01, 02, 03, 04... para esses numeros, detetar se sao abaixo de 10 e permitir que o argumento dado a 'ex' seja so com 1 digito (exemplo: '$ ex 1' igual a: '$ ex 01')
-      bash $(ls | grep "^$i")
+
+      if [[ $i -lt 10 ]]; then
+         # Variavel a concatenar ao numero que for menor que 10 (exemplo: 01, 02, 03, 04)
+         # Evita usar '$ ex 01' e pode passar a usar '$ ex 1' que este software deteta o numero correto
+            v_zero="0"
+
+         # Concat 2 variables ($v_zero + $i): 
+            v_zero_plus_i="${v_zero}$i"
+
+         # Run the concatnation
+            bash $(ls | grep "^$v_zero_plus_i")
+      else
+         # Run normally if it is lower or higher than 10
+            bash $(ls | grep "^$i")
+      fi
+
+      #bash $(ls | grep "^$i")
       #python3 $(ls | grep ".py$" | grep "$i")
    done
 }
