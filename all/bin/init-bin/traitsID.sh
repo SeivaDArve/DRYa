9e71dc7070bb6c732f0a78ed3ffa258b1b404aa1
seivaDArve <flowreshe.seiva.d.arve@gmail.com>
2024-06-04 10:45:33 +0100

    Imp: .../init-bin/traitsID.sh: agora deteta o termux e exporta essa variavel

--- all/bin/init-bin/traitsID.sh
+++ all/bin/init-bin/traitsID.sh
@@ -4,8 +4,17 @@
    # v_array[2]="Name2"
 
 
-
 # Detetar o Termux
-   #/data/data/com.termux/files/usr
-# v_is_termux="True'
-# export v_is_termux
+   # No termux a variavel $PREFIX nao vem vazia e tras normalmente o conteudo: "/data/data/com.termux/files/usr"
+   if [[ $PREFIX == "/data/data/com.termux/files/usr" ]]; then
+      #echo "Estamos no termux"  # Debug
+      traits_termux="true"
+      export traits_termux
+   else
+      traits_termux="false"
+      export traits_termux
+   fi
+      
+
+# Catalogo de todas as variaveis do traitsID encontradas neste ficheiro desde o inicio
+   # traits_termux = [ 'true' | 'false' ]
