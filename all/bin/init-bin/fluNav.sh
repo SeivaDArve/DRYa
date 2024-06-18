2c04d05654de7557c0b4c727dd18bb31f45a14e8
seivaDArve <flowreshe.seiva.d.arve@gmail.com>
2024-06-19 00:25:39 +0100

    Bug fix: fluNav

--- all/bin/init-bin/fluNav.sh
+++ all/bin/init-bin/fluNav.sh
@@ -705,7 +705,7 @@ function V {
    
    # Implementation of Use 2:
    elif [ $1 == "trade" ] || [ $1 == "t" ]; then
-      cd ${v_REPOS_CENTER}/moedaz/all/trade/dashboard-scripts && ls
+      cd ${v_REPOS_CENTER}/moedaz/all/trade/Binance-Bot && ls
       #f_trade_interactive_dir
    
    # Implementation of Use 2:
