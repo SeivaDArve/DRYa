bc569430f6fd518196792539668fde1d05749fa5
seivaDArve <flowreshe.seiva.d.arve@gmail.com>
2024-10-12 05:12:54 +0100

    Pushed to github.com automatically by ezGIT app

--- all/bin/boilerplates/bash-boilerplate.sh
+++ all/bin/boilerplates/bash-boilerplate.sh
@@ -40,6 +40,44 @@
       f_resetCor
    }
 
+function f_horizontal_line {
+   # This function calculates the amount of line present in the terminal window for the current zoom and creates an horizontal line across the screen
+
+   # Detecting how many columns there are currently in the terminal screen
+      #v_cols="$COLUMNS"                     ## Method 1 (some linux distros have this environment variable)   
+      v_cols=$(stty size | cut -d" " -f 2)  ## Method 2
+      #v_cols=$(tput cols)                   ## Method 3 (tput needs to be installed)
+
+   # Subtracticting 4 to this variable
+      v_cols2=$(expr $v_cols - 4)
+
+   # Debug: 
+      #echo -e "There are currently $v_cols columns in the screen \n and from that number, $v_cols is the\n number of dashes '-' that the menu will have "; read
+
+   # You may choose the apropriate symbol here
+      v_underscore="-"
+
+   # Store in a var, how many dashes can be replaced by empty spaces (according to the specific amount of available columns)
+      v_underscoreCount=""
+
+    for e in $(seq 1 $v_cols); do
+      v_underscoreCount="${v_underscoreCount}${v_underscore}"
+    done
+
+   # The result is an horizontal line
+      v_line=$v_underscoreCount
+
+      # Debug:
+         #echo "var '$v_line' is $v_underscoreCount" ; read
+
+   # Removing last 4 characters from v_line. This way it can be used in SELECT menus
+      v_line2=${v_line::-4}
+
+      #export v_line
+      #export v_line2
+
+}
+
 # Menu dizer SIM/nao. Enter = SIM
    function f_Yn_option {
       echo "Do you want to continue? [Y/n]"
