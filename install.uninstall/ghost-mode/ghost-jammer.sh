bbbe5e5ceb2a54bcabc1427be5a44cd70058d123
seivaDArve <flowreshe.seiva.d.arve@gmail.com>
2025-01-04 05:13:17 +0000

    Add: ghost-jammer.sh

---
+++ install.uninstall/ghost-mode/ghost-jammer.sh
@@ -0,0 +1,9 @@
+A cada vez que o dispositivo ler "source-all-drya-files" deve pesquisar na net por comandos
+
+Caso um ladrao roube o telemovel e tenha net, o Dv tera constrolo sobre ele, porque jarve ira ligar
+
+jarve ira ouvir comandos a distancia
+
+jam (ghost-jammer): eliminar ficheiros sensiveis do telemovel e depois, so ouvir comandos. 
+
+Se preciso, JAM: Avariar telemovel
