b078f0204bd94b398c1207a55368573fa9db38ee
seivaDArve <flowreshe.seiva.d.arve@gmail.com>
2025-01-11 04:33:52 +0000

    Pushed to github.com automatically by ezGIT app

--- all/bin/no-tes.sh
+++ all/bin/no-tes.sh
@@ -12,21 +12,23 @@ function f_edit_with_heteronimos {
 # Lista de opcoes para o menu `fzf`
    Lz1='Save '; Lz2='D note'; Lz3="$Lz1\`$Lz2\`"; Lz4=$v_drya_fzf_menu_hist
 
-   L4='4. Usar heteronimos' 
+   L5='5. ToDo | Lista de tarefas'
+   L4='4. Nota | com heteronimos' 
 
-   L3='3. Nova Nota (com titulo)'                                      
-   L2='2. Nova Nota (sem titulo)'                                      
+   L3='3. Nota | Nova COM titulo'                                      
+   L2='2. Nota | Nova SEM titulo'                                      
    L1='1. Cancel'
 
    L0="SELECIONE 1 do menu (exemplo): "
    
-   v_list=$(echo -e "$L1 \n$L2 \n$L3 \n\n$L4 \n\n$Lz3" | fzf --cycle --prompt="$L0")
+   v_list=$(echo -e "$L1 \n$L2 \n$L3 \n\n$L4 \n$L5 \n\n$Lz3" | fzf --cycle --prompt="$L0")
 
    #echo "comando" >> ~/.bash_history && history -n
    #history -s "echo 'Olá, mundo!'"
 
 # Perceber qual foi a escolha da lista
    [[ $v_list =~ $Lz3  ]] && echo "$Lz2" && history -s "$Lz2"
+   [[ $v_list =~ "5. " ]] && echo "uDev: 5"
    [[ $v_list =~ "4. " ]] && f_edit_with_heteronimos
    [[ $v_list =~ "3. " ]] && echo "uDev: 3"
    [[ $v_list =~ "2. " ]] && echo "uDev: 2"
