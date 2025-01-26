c5771431593fa940559af7440e2271a7938569a5
seivaDArve <flowreshe.seiva.d.arve@gmail.com>
2025-01-26 15:22:11 +0000

    File added (uDev): drya-lib-4...

---
+++ all/lib/drya-lib-4-test-dependencies-1st.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Title: Test dependencies existence
+
+# Test the existence of DRYa dependencies like: fzf, figlet, ncurses-utils, etc
+#
+# See dependencies on file '1st' somewhere in DRYa repo
