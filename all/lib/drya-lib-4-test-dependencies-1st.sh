c20a5b4f463fd6ca23c49710dc7c58b042339999
seivaDArve <flowreshe.seiva.d.arve@gmail.com>
2025-02-02 16:56:41 +0000

    Imp: testing repo existence: drya lib 2

--- all/lib/drya-lib-4-test-dependencies-1st.sh
+++ all/lib/drya-lib-4-test-dependencies-1st.sh
@@ -4,3 +4,37 @@
 # Test the existence of DRYa dependencies like: fzf, figlet, ncurses-utils, etc
 #
 # See dependencies on file '1st' somewhere in DRYa repo
+
+
+
+
+function f_testing_either_repo_or_directory {
+   cd $v_repo 
+
+   if [ -d ".git" ]; then
+      echo "It is a repo"
+
+   else
+      echo "it is not a repo"
+   fi
+}
+
+function f_ensure_repo_existence {
+   # Tests if a repository exists. If it does not, it clones it
+   # Needs var: v_ensure
+
+   # uDev: ensure `git` is also installed to clone repo if needed
+
+   v_repo=${v_REPOS_CENTER}/$v_ensure
+
+   [[ -z $v_ensure ]] && echo "Could not test repo existence without specofying its name at $v_ensure" 
+
+   if [ -d $v_ensure ]; then
+      echo "it does exist, now we test if it is a repo"
+      f_testing_either_repo_or_directory
+
+   else
+      echo "it does not exist"
+   fi
+}
+f_ensure_repo_existence
