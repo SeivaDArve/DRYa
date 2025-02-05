7f51caca5ae9d034877f24c8285a5ba3b0ed0d37
seivaDArve <flowreshe.seiva.d.arve@gmail.com>
2025-02-05 17:07:30 +0000

    Improved: script that mentions absolute and relative path of a running script

--- install.uninstall/linux-or-WSL/master-bashrc/arg-0-abs-path.sh
+++ install.uninstall/linux-or-WSL/master-bashrc/arg-0-abs-path.sh
@@ -1,24 +1,58 @@
 #!/bin/bash
 # Title: Script to echo/store the name of the running script
 
-clear
-
-function f_get_script_current_abs_path {
-
-	# no matter from where we will execute this script, $SCRIPT_DIR will indicate the correct directory where this script is located
-	_SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-	echo "This script is written/located at:"; 
-	echo " > $_SCRIPT_DIR";
-	
-	function f_test1 {
-		# This does not work, it is subjective to change. it depends from where you ryn the script
-		_drya_pwd=$(pwd)
-		echo " > $_drya_pwd"
-	}
+function f_1 {
+   # This does not work, it is subjective to change. it depends from where you ryn the script
+      v_pwd=$(pwd)
+      echo "Current working directory:"
+      echo " > $v_pwd/"
+      echo
 }
 
-f_get_script_current_abs_path
+function f_2 {
+   # no matter from where we will execute this script
+   #   $SCRIPT_DIR will indicate the correct directory 
+   #   where this script is located
 
-echo
-echo "Script name:"
-echo " > $0"
+      v_script_directory=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+      echo "Path only: to the working dir of running script:"; 
+      echo " > $v_script_directory/";
+      echo
+}
+
+function f_3 {
+   # This does not work, it is subjective to change. it depends from where you ryn the script
+      echo "Full name only: of the running script:"
+      echo " > ./$0"
+      echo
+}
+
+function f_4 {
+   # This does not work, it is subjective to change. it depends from where you ryn the script
+      echo "Basename only: of the running script:"
+      v_basename=$(basename $0)
+      echo " > $v_basename"
+      echo
+}
+
+function f_5 {
+   # Joining script path and name
+      v_basename=$(basename $0)
+      v_script=$v_script_directory/$v_basename
+      echo "Absolute path to the running script:"
+      echo " > $v_script"
+      echo
+}
+
+function f_exec {
+   # Comment all not supposes to run
+
+   clear
+   f_1
+   f_2
+   f_3
+   f_4
+   f_5
+}
+f_exec
+
