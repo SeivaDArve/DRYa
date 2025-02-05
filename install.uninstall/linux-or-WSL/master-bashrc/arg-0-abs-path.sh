4af5e4a6dd26360b3b16608ad07b24d8ef3cae41
seivaDArve <flowreshe.seiva.d.arve@gmail.com>
2025-02-05 20:05:48 +0000

    Upg: Script that tells the paths and names of itself and how to reach it from anywhere

--- install.uninstall/linux-or-WSL/master-bashrc/arg-0-abs-path.sh
+++ install.uninstall/linux-or-WSL/master-bashrc/arg-0-abs-path.sh
@@ -2,52 +2,76 @@
 # Title: Script to echo/store the name of the running script
 
 function f_1 {
-   # This does not work, it is subjective to change. it depends from where you ryn the script
-      v_pwd=$(pwd)
-      echo "Current working directory:"
-      echo " > $v_pwd/"
-      echo
+   # Gives the terminal's current directory name (the directory from where, the user is calling a script. This script)
+
+   v_pwd=$(pwd)
+   v_pwd="$v_pwd/"
+
+   echo "Current working directory:"
+   echo " > $v_pwd"
+   echo
 }
 
 function f_2 {
-   # no matter from where we will execute this script
+   # Gives working directory where the script is placed (without the name ib the end)
+
+   # No matter from where we will execute this script
    #   $SCRIPT_DIR will indicate the correct directory 
    #   where this script is located
 
-      v_script_directory=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-      echo "Path only: to the working dir of running script:"; 
-      echo " > $v_script_directory/";
-      echo
+   v_script_directory=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+   v_script_directory="$v_script_directory/"
+
+   echo "Path only: to the working dir of running script:"; 
+   echo " > $v_script_directory";
+   echo
 }
 
 function f_3 {
-   # This does not work, it is subjective to change. it depends from where you ryn the script
-      echo "Full name only: of the running script:"
-      echo " > ./$0"
+   # Gives the argument number zero of the running script (The name of itself with the relative path)
+
+   # Note: example on how to use `sed` recursively to remove the text ./ at the beginning of the line until it finds none
+      # How to use:  sed ':a; s/pattern/substitution/; ta' file.txt
+      # Example:     echo "./././example-script-name.sh" | sed ':a; s/^\.\///; ta'  
+      # RESULT:      example-script-name.sh
+
+   # Ensuring only 1x the text ./ appears at the beginning of the line
+      v_name=$(echo "$0" | sed ':a; s/^\.\///; ta')
+      v_name="./$v_name"
+
+      echo "Arg 0, given at the terminal as the running script (relative path):"
+      echo " > $v_name"
       echo
 }
 
 function f_4 {
-   # This does not work, it is subjective to change. it depends from where you ryn the script
-      echo "Basename only: of the running script:"
-      v_basename=$(basename $0)
-      echo " > $v_basename"
-      echo
+   # Give only the name of the script itself without any path at all
+
+   v_basename=$(basename $0)
+
+   echo "Basename only: of the running script:"
+   echo " > $v_basename"
+   echo
 }
 
 function f_5 {
-   # Joining script path and name
-      v_basename=$(basename $0)
-      v_script=$v_script_directory/$v_basename
-      echo "Absolute path to the running script:"
-      echo " > $v_script"
-      echo
+   # Joining script directory name and basic last name
+
+   v_script_directory=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+   v_basename=$(basename $0)
+   v_script=$v_script_directory/$v_basename
+
+   echo "Absolute path to the running script:"
+   echo " > $v_script"
+   echo
 }
 
 function f_exec {
-   # Comment all not supposes to run
+   # Joining all possibilities is a sequence
 
+   # Comment all not supposes to run
    clear
+
    f_1
    f_2
    f_3
