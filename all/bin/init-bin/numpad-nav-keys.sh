c146a58f92b7df94327cff874bc9882d878e3d39
Seiva_MSI_Leopard_pro <flowreshe.seiva.d.arve@gmail.com>
2025-04-01 22:27:22 +0100

    Add: script: numpad-nav-keys

---
+++ all/bin/init-bin/numpad-nav-keys.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Title: Numpad navigation keys
+# Description: Using only the Numapad to do the same as commands. Improving speed
+
+# For DRYa:
+   alias 5="drya"
+
+# For ezGIT
+   alias 1="G"
