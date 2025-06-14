fa4aefe3ddcad494eafad8c67aa65c7137f6aa9c
Seiva_Android_OPPO_Mike_2 <flowreshe.seiva.d.arve@gmail.com>
2025-06-14 02:20:47 +0100

    Imp: Info on how to run elisp directly from the terminal

--- all/bin/elisp-terminal-script-example.el
+++ all/bin/elisp-terminal-script-example.el
@@ -3,10 +3,15 @@
 ;; Description:
 ;;    This is a one line of comment
 ;; 
-;;    In order to run this script from the temrinal, type:
+;;    In order to run this script from the terminal, type:
 ;;    emacs --script <name-of-file-containing-elisp>
 ;;
 ;;    You can also create an alias to "emacs --script", for example:
-;;     > alias emc="emacs --script"
+;;     > alias ems="emacs --script"
+;;
+;;    You can also put an emacs script in a directory mentioned by the variable $PATH like /bin and change its mode to executable `chmod +x <emacs-script-name>`
+;;    Then, call it direclty (with autocompletion, due to $PATH being set) on the prompt with:
+;;     > `emacs-script-name` 
+
 
 (message "Hello World")
