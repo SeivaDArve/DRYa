d48fdfbca555459c1572607daa6b1faeead29dff
Seiva_MSI_Leopard_pro <flowreshe.seiva.d.arve@gmail.com>
2025-06-30 10:05:25 +0100

    Upg: new drya-lib-6 for flash buffers

---
+++ all/lib/drya-lib-6-flash-buffer.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# Title: drya-lib-6-flash-buffer
+# Description: Abrir temporariamente uma buffer com mensagens de DRYa (em buffer a parte, tal como o `vim` faz)
+
+# Entra no buffer alternativo
+   tput smcup
+
+# Move para o canto superior esquerdo
+   tput cup 0 0
+
+v_talk="DRYa: buffer: "
+
+# Apresentacao
+   [[ -z $v_fig ]] && v_fig="INFO"
+   
+   figlet $v_fig
+
+# Explicar ao user o que se passa
+   echo "$v_talk Mensagem Temporaria (buffer) "
+
+# Texto a apresentar
+   [[ -z $v_txt ]] && v_txt="Qual é a info que quer apresentar?? "
+
+   echo
+   echo $v_txt
+   echo 
+
+# Aguarda 5 segundos
+   #tput cr → move o cursor para o início da linha.
+   #tput el → limpa da posição atual até o fim da linha.
+   #tput cuu1 move o cursor 1 linha para cima
+   #echo -n → imprime sem pular linha.
+
+   tput cr #→ move o cursor para o início da linha.
+   tput el #→ limpa da posição atual até o fim da linha.
+   #echo -n    "$v_talk Pressione ENTER (.  )  "; read
+   read -s -p    "$v_talk Pressione ENTER (.  )  " v_ans
+   echo "$v_ans"
+   read
+   tput cuu1
+
+   tput cr #→ move o cursor para o início da linha.
+   tput el #→ limpa da posição atual até o fim da linha.
+   #echo -n    "$v_talk Pressione ENTER (.. )  "; read
+   read -s -p    "$v_talk Pressione ENTER (.. )  "
+   tput cuu1
+
+   tput cr #→ move o cursor para o início da linha.
+   tput el #→ limpa da posição atual até o fim da linha.
+   read -s -p    "$v_talk Pressione ENTER (...)  "
+   tput cuu1
+
+   # Opcional: move para a próxima linha depois
+   echo
+
+
+# Sai do buffer alternativo, restaurando a tela anterior
+   tput rmcup
