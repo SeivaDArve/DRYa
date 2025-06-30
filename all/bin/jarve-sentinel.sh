b6ef3d623957410914766834fcc5ad9ccf025137
Seiva_MSI_Leopard_pro <flowreshe.seiva.d.arve@gmail.com>
2025-06-30 10:07:46 +0100

    Imp: jarve

--- all/bin/jarve-sentinel.sh
+++ all/bin/jarve-sentinel.sh
@@ -20,10 +20,14 @@
 # uDev: se existir a repo 'verbose-lines': atualizar de X em X tempos
 
 echo "Jarve: running (uDev list)"
+echo 
+echo " > Este script serve para testar a existencia da repo 'Jarve' com drya-lib-4 e esclarecer"
+echo "   que esta repo vai incluir agenda + calendario + relogio + lembretes (assincrono)"
+echo
 echo " > jarve sets up everything after laptop suspention ends"
 echo " > ASCII art to know from a distance changes are being made"
-echo " > Every time changes are detected, play a sound"
-echo " > Every time changes are finished, play a sound"
+echo " > Every time changes are detected, play a sound (differente sounds)"
+echo " > Every time changes are finished, play a sound (different sounds)"
 echo " > Detetar ano/mes/dia/hora/minuto constantemente para apps de calendario tipo:"
 echo " > moedaz: compromissos agendados \"source-all-moedaz-files\""
 echo " > moedaz: trade: \"G-Midnight-balance-of-day-checker.py\""
@@ -37,15 +41,27 @@ echo "   >> enviar inicialmente ambos os IP pronto para SSH para o Verbose-lines
 echo "   >> Git Pull a essa repo (caso seja o raspberry, e a momolada esteja encarregue de carregar "
 echo "   >> no botao ON, ja nao tem de enviar por whatsapp nenhuma info pra mim). Serve para o SSHFS"
 echo 
+echo
+echo
+echo  "Este comando mostra:"
+echo  "|----------------------|"
+echo  "| Jarve daemon: On/Off |"
+echo  "|----------------------|"
+echo
+echo
+echo  "[ANY KEY] para executar o sentinel"
 
-read -p "[ANY KEY] para executar o sentinel"
 while true
 do
-   echo "Starting ezGIT pull all"
-   bash ${v_REPOS_CENTER}/ezGIT/ezGIT.sh v all &>/dev/null
-   echo "ended at <date> (waiting 5 min to restart)"
-   # uDev: sound at the end
-   sleep 1
+
+   # A cada 5 minutos
+      echo "Starting ezGIT pull all (exemplo)"
+      #bash ${v_REPOS_CENTER}/ezGIT/ezGIT.sh v all &>/dev/null
+      echo "ended at <date> (waiting 5 min to restart)"
+      # uDev: sound at the end
+      sleep 60*5
+
+   # A cada 
 
 done
 
