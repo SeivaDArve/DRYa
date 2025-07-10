0620fb8164a1810a71a932a9cf4217aa411935e8
Seiva_Android_OPPO_Mike_2 <flowreshe.seiva.d.arve@gmail.com>
2025-07-10 06:56:32 +0100

    Imp text: drya-lib-2

--- all/lib/drya-lib-2-tmp-n-config-files.sh
+++ all/lib/drya-lib-2-tmp-n-config-files.sh
@@ -14,7 +14,7 @@
 #        v_lib2=${v_REPOS_CENTER}/DRYa/all/lib/drya-lib-2-tmp-n-config-files.sh
 #        [[ -f $v_lib2 ]] && source $v_lib2 || read -s -n 1 -p "Error: drya-lib-2 does not exist"
 #
-#        f_create_tmp_file  # will give a $v_tmp with a new file with abs path
+#        # Example: f_create_tmp_file will create a temporary file stored at $v_tmp (with abs path, at ~/.tmp/...)
 #     
 #     
 
