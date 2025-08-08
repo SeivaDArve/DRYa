8e197fe48962f38ef476ff620d8a88c0a2c605d9
Seiva_Android_A6_plus <flowreshe.seiva.d.arve@gmail.com>
2025-08-08 21:41:53 +0100

    Pushed to github.com automatically (by ezGIT app)

--- all/bin/init-bin/fluNav.sh
+++ all/bin/init-bin/fluNav.sh
@@ -1396,7 +1396,7 @@ function V {
             vim $v_fluNav_V_hist_file
 
       else 
-         echo DRYa: fluNav: V: file/dir/option not found. Found this instead: $v_found
+         echo DRYa: fluNav: V: file/dir/option not found. 
    fi
 
 }
