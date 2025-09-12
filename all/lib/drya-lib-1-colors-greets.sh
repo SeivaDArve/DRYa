49213c6846f503031016423814b62e569bc83528
Seiva_ASUS_15_OLED <flowreshe.seiva.d.arve@gmail.com>
2025-09-12 03:08:08 +0100

    Upg: from drya-lib-1 now the fx f_hzl can read $1 and the v_hzl variable

--- all/lib/drya-lib-1-colors-greets.sh
+++ all/lib/drya-lib-1-colors-greets.sh
@@ -326,9 +326,14 @@ function f_hzl {
 
    # If the script that used this library does not set the variable that fills the line, then the character is set as default here
       [[ -z $v_hzl ]] && v_hzl="-"
+   
+   # Allowing $1 to overwrite $v_hzl
+      [[ -n $1 ]] && v_hzl=$1
       
    v_cols=$(tput cols)
    printf "%*s" $v_cols | tr " " "$v_hzl"
+
+   unset v_hzl  # Obrigar a que as fx externas tenham sempre de configurar esta variavel para que ela saia do padrao default
 }
 
 function f_horizline {
