1eaae80d4952bb70b623e2e8c5ea4ba98dfb3dea
publicDVC_pi3_raspberryOS_vascogama <flowreshe.seiva.d.arve@gmail.com>
2026-02-04 12:03:26 +0000

    Imp: drya-keyboard with hotkey info

--- all/bin/fzf-keyboard-alterbative/run-fzf-keyboard.sh
+++ all/bin/fzf-keyboard-alterbative/run-fzf-keyboard.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-L0="drya-emergency-keyboard: Cola uma letra no cursor: "
+L0='drya-emergency-keyboard: `Ctrl-x` Cola uma letra no cursor: '
 CHOICE=$(printf "a\nb\nc\n?\n\\ \n^\n@" | fzf --cycle --prompt="$L0")
 echo -n "$CHOICE"
