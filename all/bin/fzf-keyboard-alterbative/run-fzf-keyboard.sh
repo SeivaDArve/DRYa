34ffea9904e9bbc6e31676c01ef104deeea49d08
publicDVC__pi3__raspberryOS__vascogama <flowreshe.seiva.d.arve@gmail.com>
2026-05-18 18:48:21 +0100

    Upg: DRYa Emergency Keybvoard: Now it has all keys (all lowercase and uppercase letters. Also all numbers and all symbols)

--- all/bin/fzf-keyboard-alterbative/run-fzf-keyboard.sh
+++ all/bin/fzf-keyboard-alterbative/run-fzf-keyboard.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-L0='drya-emergency-keyboard: `Ctrl-x` Cola uma letra no cursor: '
-CHOICE=$(printf "a\nb\nc\n?\n\\ \n^\n@\n-\n/" | fzf --cycle --prompt="$L0")
+Lh=$(echo -e "\nNotas:\n - Escolhe desta lista uma LETRA para imprimir no prompt\n - Abrir este menu: \`Ctrl-X\` \n - A seta [BAIXO] navega para o final da lista \n - Esta lista serve para teclados desconfigurados ou teclas INOP ")
+L0='DRYa: Emergency Keyboard: [ENTER] para colar uma letra: '
+CHOICE=$(printf "a\nb\nc\nd\ne\nf\ng\nh\ni\nj\nk\nl\nm\nn\no\np\nq\nr\ns\nt\nu\nv\nw\nx\ny\nz\n \nA\nB\nC\nD\nE\nF\nG\nH\nI\nJ\nK\nL\nM\nN\nO\nP\nQ\nR\nS\nT\nU\nV\nW\nX\nY\nZ\n \n1\n2\n3\n4\n5\n6\n7\n8\n9\n0\n \n/\n\\ \n|\n!\\n?\n^\n@\n(\n)\n[\n]\n{\n}\n=\n&\n$\n*\n+\n\"\n'\n-\n_\n.\n:\n,\n;\n~ " | fzf --no-info --cycle --header="$Lh" --prompt="$L0")
 echo -n "$CHOICE"
